297e2b634388bf58f39b4cdba1f91935772c13a5
Nata Nael <natanael280198@gmail.com>
2021-04-10 01:21:06 +0700

    Add google login

--- src/Login.js
+++ src/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { login } from './features/userSlice'
-import { auth } from './firebase'
+import { auth, provider } from './firebase'
 import './Login.css'
 
 function Login() {
@@ -49,6 +49,19 @@ function Login() {
         .catch(error => alert(error))
     }
 
+    const loginGoogle = () => {
+        auth.signInWithPopup(provider)
+        .then((userAuth) => {
+            dispatch(login({
+                email: userAuth.user.email,
+                uid: userAuth.user.uid,
+                displayName: name,
+                photoURL: profilePic,
+            }))
+        })
+        .catch((error) => alert(error.message));
+    }
+
     return (
         <div className="login">
             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/600px-LinkedIn_logo_initials.png" alt="Header Logo"/>
@@ -61,6 +74,8 @@ function Login() {
 
                 <button onClick={loginToApp}>Login</button>
                 <p>Don't have an account? Register <span className="login__register" onClick={register}>Here</span></p>
+                <p>Other Login options:</p>
+                <img className="login__googleImage" alt="Google Login" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/1200px-Google_%22G%22_Logo.svg.png" onClick={loginGoogle}></img>
             </form>
         </div>
     )
